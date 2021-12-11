84569f797d4fc5d72972498a94e3d8a49df9ce43
Leprechaun817 <2672962+Leprechaun817@users.noreply.github.com>
2021-12-11 16:42:54 -0500

    Added licensing to relevant code

--- TaskerMainWindow.cs
+++ TaskerMainWindow.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//		Name of Program/Application: Tasker
+//		Copyright (C) 2021  Aaron Gagern
+//
+//		This program is free software: you can redistribute it and/or modify
+//		it under the terms of the GNU General Public License as published by
+//		the Free Software Foundation, either version 3 of the License, or
+//		(at your option) any later version.
+//
+//		This program is distributed in the hope that it will be useful,
+//		but WITHOUT ANY WARRANTY; without even the implied warranty of
+//		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//		GNU General Public License for more details.
+//
+//		You should have received a copy of the GNU General Public License
+//		along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Threading;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -1013,8 +1033,6 @@ namespace Tasker
 			if(IsDebugModeEnabled)
 				Warn("\nTasker Force Close Timestamp (in Minutes): " + currentTimeInMins.ToString());
 
-			SaveTaskerProgramState();
-
 			CancelAndDisposeBackgroundWorker();
 
 			if(IsDebugModeEnabled)
@@ -1047,26 +1065,6 @@ namespace Tasker
 
 				timeTrackingLoop.Dispose();
 			}
-		}
-
-		private void SaveTaskerProgramState()
-		{
-			//Save timer states
-			byte[] timeStateArr = tS.SerializeToByteArray();
-			Debug(timeStateArr.ToString()!);
-
-			//Save entry queue
-			byte[] entryQueueArr = entryQueue.SerializeToByteArray();
-			Debug(entryQueueArr.ToString()!);
-
-			//Save listViewOperations state
-			byte[] lvOpsArr = lvOps.SerializeToByteArray();
-			Debug(lvOpsArr.ToString()!);
-
-			//Save error provider states
-			string startTimeTextBoxErrorState = startTimeErrorProvider.GetError(startTimeTextBox);
-			string endTimeTextBoxErrorState = endTimeErrorProvider.GetError(endTimeTextBox);
-			string remindTimeTextBoxErrorState = remindIntervErrorProvider.GetError(remindIntervTextBox);
 		}
 
 		private int GetStartTimeInMinutes()
